c825a9dac92263712cf2086b291ecefd2ad0853b
Don11235 <106115145+Don11235@users.noreply.github.com>
2025-02-03 23:17:34 +0100

    Update lcm.c

--- exam_rank_2/level3/lcm.c
+++ exam_rank_2/level3/lcm.c
@@ -15,7 +15,7 @@ unsigned int hcf(unsigned int a, unsigned int b)
 
 unsigned int lcm(unsigned int a, unsigned int b)
 {
-	if (a == 0 || b == 0)
+	if ((int)a <= 0 || (int)b <= 0)
 		return (0);
-	return ((a * b) / hcf(a, b));
+	return ((a / hcf(a, b)) * b);
 }
